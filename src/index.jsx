8d2a8f3be0ea892a9f3363675c895a3d22326078
xiaolong666666 <long.1585097369@qq.com>
2024-05-22 19:36:31 +0800

    feat: react native init

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import {
+  Text,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <View>
+        <Text>index</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
